d93da83499c7de83a53565c686a4e14207d8b20f
SOLVE-NITK <csd@nitk.edu.in>
2024-08-12 15:05:42 +0530

    updated mom1 experiments

---
+++ js/graph_line_use.ob.js
@@ -0,0 +1,58 @@
+var _$_85d1 = [
+  "top",
+  "center",
+  "bold",
+  "calibri",
+  "dimGrey",
+  "line",
+  "Float",
+  "f(x)",
+  "Chart",
+  "render",
+];
+function graphline(_0xD9B9, _0xD9A9, _0xD9C9, _0xD9D9) {
+  var _0xD999 = new CanvasJS[_$_85d1[8]](_0xD9B9, {
+    backgroundColor: "transparent",
+    zoomEnabled: true,
+    toolTip: { shared: false },
+    legend: {
+      verticalAlign: _$_85d1[0],
+      horizontalAlign: _$_85d1[1],
+      fontSize: 14,
+      fontWeight: _$_85d1[2],
+      fontFamily: _$_85d1[3],
+      fontColor: _$_85d1[4],
+    },
+    axisY: {
+      title: _0xD9D9,
+      includeZero: false,
+      interval: 1, // Ensures that only integer values are displayed on the Y-axis
+      valueFormatString: "#0", // Formats the Y-axis labels as integers
+    },
+    axisX: {
+      title: _0xD9C9,
+      interval: 1, // Ensures that only integer values are displayed on the X-axis
+      valueFormatString: "#0", // Formats the X-axis labels as integers
+    },
+    data: [
+      {
+        type: _$_85d1[5],
+        xValueType: _$_85d1[6],
+        showInLegend: true,
+        name: _$_85d1[7],
+        markerSize: 10, // Markers are hidden by default
+        toolTipContent: "{a<sub>n</sub>}: {n}", // Displays the tooltip with markers
+        dataPoints: _0xD9A9,
+        mouseover: function (e) {
+          e.dataSeries.markerSize = 5;
+          e.chart.render();
+        }, // Show markers on hover
+        mouseout: function (e) {
+          e.dataSeries.markerSize = 0;
+          e.chart.render();
+        }, // Hide markers when not hovering
+      },
+    ],
+  });
+  _0xD999[_$_85d1[9]]();
+}
---
+++ js/script.js
@@ -0,0 +1,65 @@
+/***
+ * Concept: An arithmetic progression is a sequence of numbers in which the difference between consecutive terms is constant.
+ * Formula: a_n =a_1 + (n-1) * d
+ *a_n is the nth term
+ *a_1 is the first term
+ *d is the common difference
+ *n is the position of term in the sequence
+ */
+
+let a1 = 3,
+  d = 2;
+n = 3;
+let anext = a1;
+
+let arithmeticSeriesArray = [];
+document
+  .querySelector("input#firstTermSlider")
+  .addEventListener("change", function (e) {
+    a1 = Number(e.target.value);
+    document.querySelectorAll(".a1").forEach((a) => (a.textContent = a1));
+    calculateArithemticSeries();
+  });
+document
+  .querySelector("input#commonDifferenceSlider")
+  .addEventListener("change", function (e) {
+    d = Number(e.target.value);
+    document.querySelectorAll(".d").forEach((dd) => (dd.textContent = d));
+    calculateArithemticSeries();
+  });
+document
+  .querySelector("input#numberTermsSlider")
+  .addEventListener("change", function (e) {
+    n = Number(e.target.value);
+    document.querySelectorAll(".n").forEach((nn) => (nn.textContent = n));
+    calculateArithemticSeries();
+  });
+
+function calculateArithemticSeries() {
+  arithmeticSeriesArray = [];
+  anext = a1; // console.log(an);
+  arithmeticSeriesArray.push({ x: 1, y: anext });
+  let an = a1 + (n - 1) * d;
+  for (let i = 2; i <= n; i++) {
+    anext = anext + d;
+    arithmeticSeriesArray.push({ x: i, y: anext });
+  }
+  document.querySelector(".an").textContent = an;
+  document.querySelector(".ap").textContent = "";
+  arithmeticSeriesArray.map((ap, index) => {
+    document.querySelector(".ap").innerHTML +=
+      `${ap.y}` +
+      `${!(index === arithmeticSeriesArray.length - 1) ? ", " : ""}`;
+    // document.querySelector(
+    //   ".ap"
+    // ).innerHTML += `(a<sub>${ap.x}</sub> = ${ap.y}),`;
+  });
+  plotGraph();
+}
+function plotGraph() {
+  console.log(arithmeticSeriesArray);
+  document.getElementById("graph").innerHTML = "";
+  graphline("graph", arithmeticSeriesArray, "n", "an", "red");
+}
+
+calculateArithemticSeries();
